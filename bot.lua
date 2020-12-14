fb0642b2abaad5ab420c82686044f99da0a876c0
Keanu <keanutimmermans8@gmail.com>
2020-12-14 08:23:44 +0000

    Add say command.

--- bot.lua
+++ bot.lua
@@ -3,19 +3,26 @@ local client = discordia.Client {
     logFile = 'bot.log',
 	cacheAllMembers = true,
 }
+discordia.extensions() -- Load useful extensions
 
 client:on("ready", function() -- when bot sends ready event
-	print("Logged in as " .. client.user.username)
+	p(string.format('Logged in as %s', client.user.username))
 end)
 
 client:on("messageCreate", function(message)
 
 	local content = message.content
+	local args = message.content:split(' ')
 
 	if content == "l!ping" then
 		message:reply("Pong!")
 	elseif content == "l!pong" then
 		message:reply("Ping!")
+	end
+
+	if content == 'l!say' then
+		table.remove(args, 1) -- Remove l!say from args
+		message:reply(table.concat(args, ' '))
 	end
 
 end)
